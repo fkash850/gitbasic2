693ece2d0abac88cc792d9e672d92662a1967f06
Fahim Kashem <fkashem85@outlook.com>
2020-03-31 14:52:53 -0400

    added an interesting function

--- hello.java
+++ hello.java
@@ -1,4 +1,8 @@
 public class hello {
+	public void doSomethingInteresting() {
+		// Doing interesting stuff here
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Hello Git");
 	}
