efe40976573e6bc570432fdb14f723962788a8f3
Fahim Kashem <fkashem85@outlook.com>
2020-04-02 14:29:24 -0400

    some code to create merge conflict

--- hello.java
+++ hello.java
@@ -1,6 +1,11 @@
 public class hello {
 	public void doSomethingInteresting() {
 		// Doing interesting stuff here
+		int a = 10;
+		int b = 20;
+
+		a = a - b;
+		a = a * b;
 	}
 
 	public static void main(String[] args) {
